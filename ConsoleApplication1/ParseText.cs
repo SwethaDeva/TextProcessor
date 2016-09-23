7d3fe9c1318ce6c7fcaf289ee40bae6ad518e99d
SwethaDeva <jobs4swetha.deva@gmail.com>
2016-09-22 19:28:56 -0500

    ConsideringSpecialCharsAttheEnd

--- ConsoleApplication1/ParseText.cs
+++ ConsoleApplication1/ParseText.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TextProcessor
 {
@@ -50,16 +51,36 @@ namespace TextProcessor
             int index_Of_lastChar = 0;
             int count_Of_chars = 0;
             string delimiter = " ";
-
+            int index_of_spclChar = 0;
+            char[] SpecialChars = "!@#$%^&*(),';.<>".ToCharArray();
             foreach (string str in ExtractWords)
             {
-               if(str.Length>1)
+               if(str.Trim().Length>1)
                {
-                    index_Of_lastChar = str.Length - 2;
-                    string tempstr = str.Substring(1, index_Of_lastChar);
-                    count_Of_chars = tempstr.Distinct().Count();
-                    CompleteText.Append(delimiter);
-                    CompleteText.Append(str[0].ToString() + count_Of_chars + str[(str.Length) - 1].ToString());                    
+                    index_of_spclChar = str.IndexOfAny(SpecialChars);
+                    index_Of_lastChar = str.Length - 1;
+                    if (index_of_spclChar == -1)                    
+                    {
+                                             
+                        string tempstr = str.Substring(1, index_Of_lastChar-1);
+                        count_Of_chars = tempstr.Distinct().Count();
+                        CompleteText.Append(delimiter);
+                        CompleteText.Append(str[0].ToString() + count_Of_chars + str[(str.Length) - 1].ToString());
+                    }
+                    else
+                    { 
+                     //Assuming that the special charecter is always at the end of the parsed word from the inputted text.
+                      if(index_Of_lastChar == index_of_spclChar) 
+                      {
+                            string tempstr = str.Substring(1, index_Of_lastChar - 2);
+                            count_Of_chars = tempstr.Distinct().Count();
+                            CompleteText.Append(delimiter);
+                            CompleteText.Append(str[0].ToString() + count_Of_chars + str[(str.Length) - 2].ToString() + str[(str.Length) - 1].ToString());
+                        }
+
+                    }
+
+                    
                 }
                 else 
                 {
